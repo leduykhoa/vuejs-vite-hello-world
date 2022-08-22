247ff023af7cd0699b8d2e8945399c773d48de7a
Lê Duy Khoa <leduykhoa060690@gmail.com>
2022-08-22 14:28:22 +0700

    Update icon

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -9,7 +9,7 @@ export default {
     <img
         alt="Beae"
         class="mr-auto ml-2 h-10"
-        src="./../../assets/beae.svg"
+        src="./../../assets/favicon-96x96.png"
     />
     <button
         class="mr-2 smoothing h-9 text-sm px-3.5 rounded transition text-white bg-green-600 hover:bg-green-700 active:bg-green-800 focus:outline-none"
